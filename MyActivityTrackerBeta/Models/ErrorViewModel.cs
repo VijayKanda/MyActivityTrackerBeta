fa5626633d5c7fc4487f2e3e2691879e3123f559
vijay.kandasamy <vijay.kandasamy@hotmail.com>
2018-07-17 22:41:42 -0700

    Initial commit

---
+++ MyActivityTrackerBeta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyActivityTrackerBeta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
